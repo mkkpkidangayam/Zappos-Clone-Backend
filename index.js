bfa1c218d9597f1648b7f4103f09b8f6ec9c5777
mkkpkidangayam <mkkpkdm@gmail.com>
2024-07-27 11:06:30 +0530

    Updated

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: "https://mkkp-zappos.vercel.app",
-    // origin: "http://localhost:3000",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
